04e586012e69be3c33e4795142772354b6d4674a
jared1158 <jaredlns@gmail.com>
2018-06-27 17:37:37 -0700

    progress update

--- app/src/main/java/com/jaredapps/jflixster/MovieAdapter.java
+++ app/src/main/java/com/jaredapps/jflixster/MovieAdapter.java
@@ -1,7 +1,10 @@
 package com.jaredapps.jflixster;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,7 +16,7 @@ import java.util.ArrayList;
  * Created by jared1158 on 6/27/18.
  */
 
-public class MovieAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
     //list of movies
     ArrayList<Movie> movies;
@@ -21,6 +24,32 @@ public class MovieAdapter {
     //initialize with list
     public MovieAdapter(ArrayList<Movie> movies){
         this.movies = movies;
+    }
+    //creates and inflates a new view
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // get the context and create the inflater
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //create the view using the item_movie layout
+        View movieView = inflater.inflate(R.layout.item_movie, parent, false);
+        //return new ViewHolder
+        return new ViewHolder(movieView);
+
+    }
+    //binds an inflated view to anew item
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //get the movie data at the speiciffied position
+        Movie movie = movies.get(position);
+        //populate the view with the movie data
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+    }
+    //returns the total number of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
     }
 
     //create the viewholder as a static inner class
